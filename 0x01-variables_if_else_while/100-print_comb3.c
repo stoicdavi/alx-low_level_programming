e60dc6c2e6eaa549f750f7110605e4c2ac6ed803
David Nanjila <nanjiladavid2@gmail.com>
2023-07-15 08:00:20 -0700

    rite a program that prints all possible different combinations of two digits.

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+* main - entry point to the program
+*
+* Return: 0 Whenever the program runs successfully
+*/
+int main(void)
+{
+	int outer;
+	int inner;
+
+	for (outer = 0; outer < 9; outer++)
+	{
+		for (inner = 0; inner < 10; inner++)
+		{
+			putchar(outer + '0');
+			putchar(inner + '0');
+
+			if (outer == 8 && inner == 9)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
