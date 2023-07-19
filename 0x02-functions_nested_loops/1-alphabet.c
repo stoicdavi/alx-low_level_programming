008fec727fc36a49ae4aa7e33d26924a624d59a6
Nanjila <nanjiladavid2@gmail.com>
2023-07-19 03:41:41 +0300

    program that prints _putchar, followed by a new line.

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 97; alphabet <= 122; alphabet++)
+		_putchar(alphabet);
+	_putchar('\n');
+	return (0);
+}
