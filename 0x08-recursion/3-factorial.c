080d62f7a28a472586770c3030934edb2b327aac
Nanjila <nanjiladavid2@gmail.com>
2023-08-03 00:21:08 +0300

    function that returns the factorial of a given number.

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -17,6 +17,4 @@ int factorial(int n)
 	}
 	else
 		return (n * factorial(n - 1));
-	
 }
-
---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+  * _pow_recursion - fuction to return x raised to y
+  * 
+  *
+  */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else
+		return (1)
+
