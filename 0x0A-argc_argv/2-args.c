50c2f66f813279f6dcaa5ee05fde407b3b3587f6
Nanjila <nanjiladavid2@gmail.com>
2023-08-06 00:15:38 +0300

    program that prints all arguments it receives.

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+  * main - function that prints all the arguments it receives
+  * @argc: argument counter
+  * @argv: the array/value to be printed
+  * Return: 0 on success
+  */
+int main(int argc, char **argv)
+{
+	int b;
+
+	b = 0;
+	
+	for (; b < argc; b++)
+	{
+		printf("%s\n", argv[b]);
+	}
+	printf("\n");
+	return (0);
+}
+
