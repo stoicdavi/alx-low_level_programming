e70b7daa9c1f880aa23b06ab6cfc96a8467e3132
Nanjila <nanjiladavid2@gmail.com>
2023-08-05 07:03:38 +0300

    Create a script called create_static_lib.sh that creates a static library called liball.a from all the .c files that are in the current directory.

--- 0x09-static_libraries/3-strcmp.c
+++ 0x09-static_libraries/3-strcmp.c
@@ -7,8 +7,6 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int _strcmp(char *s1, char *s2)
-	{
 		while (*s1 == *s2)
 		{
 			if (*s1 == '\0')
@@ -19,5 +17,4 @@ int _strcmp(char *s1, char *s2)
 			s2++;
 		}
 		return (*s1 - *s2);
-	}
 }
