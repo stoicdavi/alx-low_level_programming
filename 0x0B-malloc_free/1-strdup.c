e1d1dae66aae372844f29050501e1a2c75d7f762
Nanjila <nanjiladavid2@gmail.com>
2023-08-09 23:50:47 +0300

    function that returns a pointer to a 2 dimensional array of integers.

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
-  * _strdup - function that returns a pointer to a newly allocated space in memory,
+  * _strdup -function to returns a pointer to newly allocated space in memory
   * which contains a copy of the string given as a parameter.
   * @str: the initial string
   *
