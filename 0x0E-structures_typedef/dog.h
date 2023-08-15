0fda2e220db734e5bcab19c25f0fe1e88c9cfff7
Nanjila <nanjiladavid2@gmail.com>
2023-08-15 23:35:14 +0300

    Fix

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef dog_h
 #define dog_h
-
+/**
+ * struct dog - a new type: struct dog with the following elements
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ * Description: a struct that houses a string and a float
+ */
 struct dog
 {
 	char *name;
