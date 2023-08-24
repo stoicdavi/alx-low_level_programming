4e61869c564ddf0dd0fb57ee530b75a5a64389c5
Nanjila <nanjiladavid2@gmail.com>
2023-08-25 00:28:47 +0300

    function that adds a new node at the beginning of a list_t list.

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,5 +1,5 @@
 #include "lists.h"
-size_t str_len(char *str);
+size_t str_len(char *st);
 /**
  * add_node - adds a new node to the end of linked list
  * @head: double pointer to a linked list
@@ -8,10 +8,7 @@ size_t str_len(char *str);
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new, *tmp;
-
-	if (str == NULL)
-		return (NULL);
+	list_t *new;
 
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
@@ -23,29 +20,21 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	}
 	new->len = str_len(new->str);
-	new->next = NULL;
-	if (*head == NULL)
-	{
-		*head = new;
-		return (new);
-	}
-	tmp = *head;
-	while (tmp->next)
-		tmp = tmp->next;
-	tmp->next = new;
-	return (new);
+	new->next = *head;
+	*head = new;
+	return new;
 }
-size_t str_len(char *str);
+
 /**
  * str_len - finds the length of a string
  * @str: string to be checked the length
  * Return: length of string
  */
-size_t str_len(char *str)
+size_t str_len(char *st)
 {
 	size_t count = 0;
 
-	for (; str[count]; count++)
+	for (; st[count]; count++)
 		;
 	return (count);
 }
