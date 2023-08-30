ac024eb25f7a7c6985696eb3bba4f59a4a75c99b
Nanjila David <nanjiladavid2@gmail.com>
2023-08-30 07:46:57 +0300

    Trying to fix task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,13 +6,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t count = 0;
+	size_t count;
 
+	count = 0;
 	while (h != NULL)
 	{
+		count++;
 		printf("%d\n", h->n);
 		h = h->next;
-		count++;
 	}
 	return (count);
 }
