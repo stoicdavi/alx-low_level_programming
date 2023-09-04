5902a3b8d8fd15f6968e0928a9963605b0950d59
Nanjila David <nanjiladavid2@gmail.com>
2023-09-05 01:24:56 +0300

    fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,7 +7,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int op, writ;
+	int op;
 	int count = 0;
 
 	if (filename == NULL)
@@ -19,11 +19,14 @@ int create_file(const char *filename, char *text_content)
 			count++;
 	}
 
-	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	writ = write(op, text_content, count);
 
-	if (op == -1 || writ == -1)
+	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	/*if (op == -1)
+		return (-1);*/
+	write(op, text_content, count);
+	/*if ((write(op, text_content, count)) == -1)
 		return (-1);
+		*/
 
 	close(op);
 
