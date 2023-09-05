b6752194dd33a96408e458434089234a58384752
Nanjila David <nanjiladavid2@gmail.com>
2023-09-05 21:37:01 +0300

    function that appends text at the end of a file.

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,41 @@
+#include "main.h"
+size_t strilen(char *str);
+/**
+ * append_test_to_file - funtion that appends text at the end of the file
+ * @filename: name of the file to be appended into
+ * @text_content: the text to appended into tbe filename file
+ * Return: 1 on success and -1on failure
+ * don't create a file if it does not exist
+ * returns -1 if i dont have the right permision
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	ssize_t writ;
+	int op;
+
+
+	if (filename == NULL)
+		return (-1);
+	op = open(filename, O_WRONLY | O_APPEND);
+	if (op == -1)
+		return (-1);
+	if (text_content != NULL)
+		writ = write(op, text_content, strilen(text_content));
+	close(op);
+	if (writ == -1)
+		return (-1);
+	return (1);
+}
+/**
+ * strlen -funtion to return the length of a string
+ * @str: the given string
+ * Return: the length of the string
+ */
+size_t strilen(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+		i++;
+	return (i);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,7 +7,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <sys/types.h>
-
+#include <string.h>
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
